7f8c07b333ab8a74d91db7f8967ff6debfaffa7b
Easton Barney <emanbarney@gmail.com>
2022-10-28 17:53:12 -0600

    initial setup/wireframe

---
+++ OneDrive/Documents/Assignments/ObjectOriented/EvolutionModel/Evolution.java
@@ -0,0 +1,42 @@
+
+
+
+public class Evolution{
+    public class Creature{
+        public static class creatureType{
+            String creatureName;
+            int count;
+        }
+    
+        public class Action{
+            private creatureCount(){
+                
+            }
+            private createCreature(){
+                Creature newCreature = new Creature();
+            }
+        }
+    
+    }
+
+
+    
+    private class Chances{
+        float spawnChance;
+        float deathChance;
+        float reproduceChance;
+        public trySpawn(){
+        
+        }
+        public tryDeath(){
+            
+        }
+        public tryReproduce(){
+            
+        }
+    }
+   
+    public static void main(String[] args){
+
+    }
+}
